aabd6ecc03b7e420f6fdc7cb7352289cf1b43558
ashesh <ashesh276@gmail.com>
2021-07-05 10:00:56 +0800

    formatting done

--- debug.py
+++ debug.py
@@ -1,39 +1,34 @@
-from dataclasses import dataclass, field
-import matplotlib.pyplot as plt
-import io
 import csv
+import functools
+import importlib
+import inspect
+import io
+import itertools
+import os
+from dataclasses import dataclass, field
+
+import matplotlib
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
-import matplotlib
-import importlib
-import os
-import functools
-import itertools
-import torch
-
-import torch.nn as nn
-import numpy as np
 import tensorflow as tf
 import tensorflow_datasets as tfds
 import tensorflow_gan as tfgan
+import torch
+import torch.nn as nn
 import tqdm
-import io
-import inspect
+
 sns.set(font_scale=2)
 sns.set(style="whitegrid")
 
 import models
-from models import utils as mutils
-from models import ncsnv2
-from models import ncsnpp
-from models import ddpm as ddpm_model
-from models import layerspp
-from models import layers
-from models import normalization
-
 #from configs.ncsnpp import cifar10_continuous_ve as configs
 from configs.ddpm import cifar10_continuous_vp as configs
+from models import ddpm as ddpm_model
+from models import layers, layerspp, ncsnpp, ncsnv2, normalization
+from models import utils as mutils
+
 config = configs.get_config()
 
 checkpoint = torch.load('exp/ddpm_continuous_vp.pth')
@@ -46,4 +41,4 @@ x = torch.ones(8, 3, 32, 32)
 y = torch.tensor([1] * 8)
 breakpoint()
 with torch.no_grad():
-  score = score_model(x, y)
+    score = score_model(x, y)
